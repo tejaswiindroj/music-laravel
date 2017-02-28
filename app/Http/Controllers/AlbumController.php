0992b5d3556e23e295b42ae9a4f802406d88d835
Tejaswi Indroj <tejaswiindroj@gmail.com>
2017-02-28 15:12:57 +0530

    Add sort feature to lists

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -10,11 +10,9 @@ class AlbumController extends Controller
 {
     public function allWithBands(Request $request)
     {
-        if ($request->input('band') != 0) {
-            $albumWithBand = Album::with('band')->where('band_id', $request->get('band'));
-        } else {
-            $albumWithBand = Album::with('band');
-        }
-        return response()->json(['bands' => Band::all()->toArray(), 'albums' => $albumWithBand->get()->toArray()]);
+        $albumWithBand = Album::leftJoin('bands', 'bands.id', '=', 'albums.band_id');
+        $albumWithBand->orderBy($request->input('sort'), $request->input('order'));
+        if ($request->input('band') != 0) $albumWithBand = $albumWithBand->where('band_id', $request->get('band'));
+        return response()->json(['bands' => Band::all()->toArray(), 'albums' => $albumWithBand->get(['albums.*', 'bands.id as band_id', 'bands.name as band_name'])->toArray(), 'sort' => ['field' => $request->input('sort'), 'order' => $request->input('order')]]);
     }
 }
--- app/Http/Controllers/BandController.php
+++ app/Http/Controllers/BandController.php
@@ -7,7 +7,8 @@ use Illuminate\Http\Request;
 
 class BandController extends Controller
 {
-    public function all() {
-        return response()->json(Band::all()->toArray());
+    public function all(Request $request)
+    {
+        return response()->json(['sort' => ['field' => $request->input('sort'), 'order' => $request->input('order')], 'bands' => Band::orderBy($request->input('sort'), $request->input('order'))->get()->toArray()]);
     }
 }
